7055f80e6701b8dd1edd137348012bcbdcec412a
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-01-27 22:58:38 -0600

    Se elimian líneas de código no necesarias

--- creacion_grafo.m
+++ creacion_grafo.m
@@ -1,12 +1,7 @@
-clear bc d G H
-
 s = size(Matrizdeadyacencia);
 G =graph(Matrizdeadyacencia,string(1:s(1)));
 
 figure ('Name', 'Grafo_matriz');
-
-%bc_citespace = betweeness_centrality_CS(G);
-d = degree(G);
 
 [size,namenodesingiant]=giantComponent(G);
 G1 = subgraph(G,namenodesingiant);
