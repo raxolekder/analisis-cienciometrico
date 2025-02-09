44a5a069b44d7c3e39f5f125be95fc89cd8d9462
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-02-09 12:45:06 -0600

    Modificación para agregar etiquetas

--- creacion_grafo.m
+++ creacion_grafo.m
@@ -1,8 +1,9 @@
 s = size(Matrizdeadyacencia);
 G =graph(Matrizdeadyacencia,string(1:s(1)));
+G.Nodes.Etiqueta = Labels.Nodo_ID;
 
 figure ('Name', 'Grafo_matriz');
-
+plot(G);
 [size,namenodesingiant]=giantComponent(G);
 G1 = subgraph(G,namenodesingiant);
 plot(G1);
