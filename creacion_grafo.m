245afd78ed7c697de974ad703017b6aed5355f06
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-02-09 16:54:54 -0600

    Etiquetas agregadas para el componente gigante

--- creacion_grafo.m
+++ creacion_grafo.m
@@ -3,8 +3,14 @@ G =graph(Matrizdeadyacencia,string(1:s(1)));
 G.Nodes.Etiqueta = Labels.Nodo_ID;
 
 figure ('Name', 'Grafo_matriz');
-plot(G);
+
 [size,namenodesingiant]=giantComponent(G);
 G1 = subgraph(G,namenodesingiant);
-plot(G1);
+
+[~, idx] = ismember(namenodesingiant, G.Nodes.Name);
+G1.Nodes.Etiqueta = G.Nodes.Etiqueta(idx, :);
+
+p1 = plot(G1);
+%labelnode(p1, 1:numnodes(G1), G1.Nodes.Etiqueta);
+
 Cellboxes = boxcoveringmemb(adjacency(G1));
