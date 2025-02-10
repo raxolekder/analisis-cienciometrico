74f9a2b303f5443448d55f834efceb5e2b75e93d
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-02-10 00:14:28 -0600

    Se agrega la columna de burstness a la estructura

--- src/creacion_grafo.m
+++ src/creacion_grafo.m
@@ -5,10 +5,10 @@ G.Nodes.Etiqueta = Resumen.Nodo_ID;
 figure ('Name', 'Grafo_matriz');
 plot(G)
 
-bins = conncomp(G); %identificar las componentes conexas de un grafo (la variable indica el componente al que cada nodo pertenece)
+bins = conncomp(G); 
 num_componentes = max(bins); 
 
-componentes={};
+componentes={}; %genera componentes para obtener el componente con más nodos
 for i = 1:num_componentes
     [v,x]=find(bins == i);
     comp{i} = subgraph(G,x);
@@ -35,9 +35,8 @@ nodosBuscados = Resumen.ID(Resumen.Burst > 0);
 nodosBuscados = arrayfun(@num2str, nodosBuscados, 'UniformOutput', false);
 Resumen.ID = string(Resumen.ID);
 
-arreglos = struct();
-
-for i = 1:numel(componentes{1}) % 13 cajas
+arreglos = struct(); %crea estructura para almacenar cada caja como un arreglo
+for i = 1:numel(componentes{1}) 
     arreglo_grafos = {}; 
     tabla_gyn = table();
     for j = 1:numel(componentes{1,1}{1,i})
@@ -47,11 +46,25 @@ for i = 1:numel(componentes{1}) % 13 cajas
             idx = findnode(G, nodoindividual);
             if idx > 0
                 nueva_fila = table({G}, {nodoindividual}, 'VariableNames', {'Grafo', 'Nodo'});
-                tabla_gyn = [tabla_gyn; nueva_fila]; % Agregar la fila a la tabla
+                tabla_gyn = [tabla_gyn; nueva_fila];
             end
         end
     end
     arreglos.(sprintf('caja_%d', i)) = tabla_gyn;
 end
 
-arreglos = calculo_BC(arreglos);
+arreglos = calculo_BC(arreglos); %calcula betweeness centrality
+
+for i = 1:numel(componentes{1}) %agrega el valor de burstness al nodo que corresponda
+    caja = sprintf('caja_%d', i); 
+    tablaActual = arreglos.(caja); 
+    tablaActual.Burst = NaN(height(tablaActual), 1);
+    for j = 1:height(tablaActual)
+        nodo = tablaActual.Nodo(j); 
+        idx = find(strcmp(Resumen.ID, nodo), 1); 
+        if ~isempty(idx)
+            tablaActual.Burst(j) = Resumen.Burst(idx); 
+        end
+    end
+    arreglos.(caja) = tablaActual;
+end
