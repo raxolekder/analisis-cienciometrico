7421d1792dbe66820d1797243fa56e655509cd8f
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-02-09 23:16:47 -0600

    Modificación archivo xlsx y código principal

--- creacion_grafo.m
+++ creacion_grafo.m
@@ -1,47 +1,55 @@
 s = size(Matrizdeadyacencia);
 G =graph(Matrizdeadyacencia,string(1:s(1)));
-G.Nodes.Etiqueta = Labels.Nodo_ID;
+G.Nodes.Etiqueta = Resumen.Nodo_ID;
 
 figure ('Name', 'Grafo_matriz');
-
-[size,namenodesingiant]=giantComponent(G);
-G1 = subgraph(G,namenodesingiant);
-
-[~, idx] = ismember(namenodesingiant, G.Nodes.Name);
-G1.Nodes.Etiqueta = G.Nodes.Etiqueta(idx, :);
-
-p1 = plot(G1);
-%labelnode(p1, 1:numnodes(G1), G1.Nodes.Etiqueta);
-
-Cellboxes = boxcoveringmemb(adjacency(G1));
+plot(G)
 
 bins = conncomp(G); %identificar las componentes conexas de un grafo (la variable indica el componente al que cada nodo pertenece)
 num_componentes = max(bins); 
-
-figure
-plot(G)
 
 componentes={};
 for i = 1:num_componentes
     [v,x]=find(bins == i);
     comp{i} = subgraph(G,x);
-    % plot(comp{i})
     Cellboxes_Comp = boxcoveringmemb(adjacency(comp{i}));
-     if diameter(comp{i}) >= 3
+    if diameter(comp{i}) >= 3
         subgrafos_r ={};
         for r = 1:length(Cellboxes_Comp)
-        boxes = Cellboxes_Comp{r};
-        subgrafos = {};
+            boxes = Cellboxes_Comp{r};
+            subgrafos = {};
             for l = 1:length(boxes) 
-            nodos = boxes{l};
-            subgrafos{l} = subgraph(comp{i},nodos);
-            %plot(subgrafos{l})
+                nodos = boxes{l};
+                subgrafos{l} = subgraph(comp{i},nodos);
             end
             subgrafos_r{r} = subgrafos; 
         end
         componentes{i} = subgrafos_r; 
-     else
-         componentes{i} = comp{i};
-        continue;
-     end
+    else
+        componentes{i} = comp{i};
+    continue;
+ end
+end
+
+nodosBuscados = Resumen.ID(Resumen.Burst > 0);
+nodosBuscados = arrayfun(@num2str, nodosBuscados, 'UniformOutput', false);
+Resumen.ID = string(Resumen.ID);
+
+arreglos = struct();
+
+for i = 1:numel(componentes{1}) % 13 cajas
+    arreglo_grafos = {}; 
+    tabla_gyn = table();
+    for j = 1:numel(componentes{1,1}{1,i})
+        G = componentes{1,1}{1,i}{1,j};
+        for k = 1:numel(nodosBuscados)
+            nodoindividual = nodosBuscados{k,1};
+            idx = findnode(G, nodoindividual);
+            if idx > 0
+                nueva_fila = table({G}, {nodoindividual}, 'VariableNames', {'Grafo', 'Nodo'});
+                tabla_gyn = [tabla_gyn; nueva_fila]; % Agregar la fila a la tabla
+            end
+        end
+    end
+    arreglos.(sprintf('caja_%d', i)) = tabla_gyn;
 end
