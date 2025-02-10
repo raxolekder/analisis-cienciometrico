40db1621b402e59b6cdeff5825aa5134b918d75d
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-02-09 18:50:35 -0600

    Generación de componentes y etiquetas de los nodos

--- creacion_grafo.m
+++ creacion_grafo.m
@@ -14,3 +14,34 @@ p1 = plot(G1);
 %labelnode(p1, 1:numnodes(G1), G1.Nodes.Etiqueta);
 
 Cellboxes = boxcoveringmemb(adjacency(G1));
+
+bins = conncomp(G); %identificar las componentes conexas de un grafo (la variable indica el componente al que cada nodo pertenece)
+num_componentes = max(bins); 
+
+figure
+plot(G)
+
+componentes={};
+for i = 1:num_componentes
+    [v,x]=find(bins == i);
+    comp{i} = subgraph(G,x);
+    % plot(comp{i})
+    Cellboxes_Comp = boxcoveringmemb(adjacency(comp{i}));
+     if diameter(comp{i}) >= 3
+        subgrafos_r ={};
+        for r = 1:length(Cellboxes_Comp)
+        boxes = Cellboxes_Comp{r};
+        subgrafos = {};
+            for l = 1:length(boxes) 
+            nodos = boxes{l};
+            subgrafos{l} = subgraph(comp{i},nodos);
+            %plot(subgrafos{l})
+            end
+            subgrafos_r{r} = subgrafos; 
+        end
+        componentes{i} = subgrafos_r; 
+     else
+         componentes{i} = comp{i};
+        continue;
+     end
+end
