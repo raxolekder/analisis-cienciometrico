ad2f64bbbce70430d79c7fb71b6e1ff9ba61c618
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-02-09 23:53:46 -0600

    Cálculo de Betweeness Centrality con normalización CiteSpace

--- src/calculo_BC.m
+++ src/calculo_BC.m
@@ -1,18 +1,19 @@
-for i = 1:13
-    caja = sprintf('caja_%d', i);
-    for j = 1:height(arreglos.(caja))
-        grafo = arreglos.(caja).Grafo{j, 1};
-        n = numnodes(grafo);
-        par_de_nodos = n * (n - 1) / 2;
-        bc = centrality(grafo, 'betweenness');
-        nodoEvaluado = arreglos.(caja).Nodo(j);
-        idx = findnode(grafo,nodoEvaluado);
-        
-        if idx > 0
-            bc_citespace_validacion = round((bc(idx) / par_de_nodos) * 2,2);
-            arreglos.(caja).BC(j,1) = bc_citespace_validacion;
-        end
+function arreglos = calculo_BC(arreglos)
+    for i = 1:13
+        caja = sprintf('caja_%d', i);
+        for j = 1:height(arreglos.(caja))
+            grafo = arreglos.(caja).Grafo{j, 1};
+            n = numnodes(grafo);
+            par_de_nodos = n * (n - 1) / 2;
+            bc = centrality(grafo, 'betweenness');
+            nodoEvaluado = arreglos.(caja).Nodo(j);
+            idx = findnode(grafo, nodoEvaluado);
 
+            if idx > 0
+                bc_citespace_validacion = round((bc(idx) / par_de_nodos) * 2, 2);
+                arreglos.(caja).BC(j, 1) = bc_citespace_validacion;
+            end
+        end
     end
 end
 
