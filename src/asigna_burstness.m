6a34ef48c7b0b6cdcfb5f9ce0ed7d4c7f8eb1b35
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-02-10 00:14:56 -0600

    Cambio en nombre de variable

--- src/asigna_burstness.m
+++ src/asigna_burstness.m
@@ -8,10 +8,10 @@ for i = 1:13
     for j = 1:height(tablaActual)
         nodo = tablaActual.Nodo(j); 
 
-       idx = find(strcmp(EtiquetasnodosconID.ID, nodo), 1); 
+       idx = find(strcmp(Resumen.ID, nodo), 1); 
 
         if ~isempty(idx)
-            tablaActual.Burst(j) = EtiquetasnodosconID.Burst(idx); 
+            tablaActual.Burst(j) = Resumen.Burst(idx); 
         end
     end
     
