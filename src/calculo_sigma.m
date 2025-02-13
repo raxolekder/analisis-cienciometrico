11cffc9fd519f50c9683bc32881d8a12c1f13603
David Parra <50473394+raxolekder@users.noreply.github.com>
2025-02-12 21:51:18 -0600

    Se crea función para el cálculo de Sigma

--- src/calculo_sigma.m
+++ src/calculo_sigma.m
@@ -1,13 +1,11 @@
-%cálculo de sigma
-
-for i = 1:13
-    caja = sprintf('caja_%d', i);
-    for j = 1:height(arreglos.(caja))
-    
-        valor_bc = arreglos.(caja).BC(j);
-        valor_bursntess = arreglos.(caja).Burst(j);
-        valor_sigma = (valor_bc + 1).^valor_bursntess;
-        arreglos.(caja).Sigma(j) = valor_sigma;
+function arreglos = calculo_sigma(arreglos)
+    for i = 1:numel(componentes{1})
+        caja = sprintf('caja_%d', i);
+        for j = 1:height(arreglos.(caja))
+            valor_bc = arreglos.(caja).BC(j);
+            valor_burstness = arreglos.(caja).Burst(j);
+            valor_sigma = (valor_bc + 1).^valor_burstness;
+            arreglos.(caja).Sigma(j) = valor_sigma;
+        end
     end
-
 end
